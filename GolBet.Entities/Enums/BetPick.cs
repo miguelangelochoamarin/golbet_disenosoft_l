eb9ae8a67b51ff50ab02756e26bef52baa163ad7
MIGUEL ANGEL OCHOA MARIAN <miguelochoa1113948@correo.itm.edu.co>
2026-08-26 23:20:22 -0500

    migracion inicial

---
+++ GolBet.Entities/Enums/BetPick.cs
@@ -0,0 +1,14 @@
+﻿namespace GolBet.Entities.Enums
+{
+    public enum BetPick
+
+    {
+
+        Home = 0,
+
+        Draw = 1,
+
+        Away = 2
+
+    }
+}
---
+++ GolBet.Entities/Team.cs
@@ -0,0 +1,30 @@
+﻿using GolBet.Entities.common;
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace GolBet.Entities;
+
+
+
+public class Team : AuditableEntity
+
+{
+
+    [Required, MaxLength(80)]
+
+    public string Name { get; set; } = null!;
+
+
+
+    [Required, MaxLength(60)]
+
+    public string City { get; set; } = null!;
+
+
+
+    [MaxLength(300)]
+
+    public string? CrestUrl { get; set; }
+
+}
